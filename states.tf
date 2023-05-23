7324ab5ea5bcd6340e8f480aabece105530ec3b4
SebastianDeLaCruzInsbuilt <134100738+SebastianDeLaCruzInsbuilt@users.noreply.github.com>
2023-05-22 22:39:25 -0500

    Create states.tf

---
+++ states.tf
@@ -0,0 +1,12 @@
+terraform{
+    backend "s3" {
+        bucket = "states-rds-pipeline-prod3828"
+        region = "us-east-1"
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+    access_key = "AKIAYBXBCVSSDL2UPX3R"
+    secret_key = "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"
+}
